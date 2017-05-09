3103af9134e05cbdc92c3ea15afa9bd13f41f4b4
EnsekiTT <ensekitt@gmail.com>
2017-05-09 21:00:28 +0900

    データ読み込み部分追加

--- README.md
+++ README.md
@@ -5,3 +5,6 @@ Floydを使って見るために試行錯誤するためのリポジトリ。
 ## Floydとは？
 https://www.floydhub.com
 
+## 計算時間短くて済みそうなirisの例を使う
+https://github.com/tensorflow/tensorflow/blob/master/tensorflow/examples/learn/iris.py
+
--- learn.py
+++ learn.py
@@ -17,34 +17,62 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-from sklearn import datasets
-from sklearn import metrics
 from sklearn import model_selection
+
+import pandas as pd
+import numpy as np
+from bunch import Bunch
 
 import tensorflow as tf
 
+
 def load_iris():
-    pass
+    """
+    $ floyd run --data GY3QRFFUA8KpbnqvroTPPW:training "python script.py"
+    みたいな書き方で/trainingにマウントされるらしい。
+    dataは[[a,b,c,d],[a,b,c,d],...,[a,b,c,d]]
+    targetは[0,0,...,2]
+    target_names = ["setosa", "versicolor", "virginica"]    
+    """
+
+    iris = pd.read_csv("dataset/iris.data", header=None)
+    iris = iris.replace({"Iris-setosa": 0, "Iris-versicolor": 1, "Iris-virginica": 2})
+    dataset = Bunch()
+    dataset.data = iris.as_matrix([0, 1, 2, 3]).astype(np.float32)
+    dataset.target = np.array(iris[4])
+
+    return dataset
 
 
-def main(unused_argv):
-    # Load dataset.
+def main(args):
+    # Load dataset
     iris = load_iris()
     x_train, x_test, y_train, y_test = model_selection.train_test_split(
       iris.data, iris.target, test_size=0.2, random_state=42)
 
     # Build 3 layer DNN with 10, 20, 10 units respectively.
-    feature_columns = tf.contrib.learn.infer_real_valued_columns_from_input(
-      x_train)
-    classifier = tf.contrib.learn.DNNClassifier(
-      feature_columns=feature_columns, hidden_units=[10, 20, 10], n_classes=3)
+    feature_columns = [tf.contrib.layers.real_valued_column("", dimension=4)]
+    classifier = tf.contrib.learn.DNNClassifier(feature_columns=feature_columns,
+                                                hidden_units=[10, 20, 10],
+                                                n_classes=3,
+                                                model_dir="iris_model")
+
+    def get_train_inputs():
+        x = tf.constant(x_train)
+        y = tf.constant(y_train)
+        return x, y
 
     # Fit and predict.
-    classifier.fit(x_train, y_train, steps=200)
-    predictions = list(classifier.predict(x_test, as_iterable=True))
-    score = metrics.accuracy_score(y_test, predictions)
-    print('Accuracy: {0:f}'.format(score))
+    classifier.fit(input_fn=get_train_inputs, steps=200)
+
+    def get_test_inputs():
+        x = tf.constant(x_test)
+        y = tf.constant(y_test)
+        return x, y
+
+    score = classifier.evaluate(input_fn=get_test_inputs, steps=1)
+    print("\nTest Accuracy: {0}\n".format(score['accuracy']))
 
 
 if __name__ == '__main__':
-  tf.app.run()
+    tf.app.run()
